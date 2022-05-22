11e1188dee7e48b405086ab6bbb5e437705e7206
victordrumond <victordrumond@hotmail.com>
2022-05-22 16:17:34 -0300

    Add sortData method to Helper

--- client/src/Helper.js
+++ client/src/Helper.js
@@ -38,6 +38,25 @@ class Helper {
         return listName.replace(' ', '').replace(listName[0], listName[0].toLowerCase());
     };
 
+    // Apply filtering to data
+    static sortData = (data, config) => {
+        if (config.filtering === 'title') {
+            return data.sort((a, b) => a.data.title.localeCompare(b.data.title));
+        }
+        if (config.filtering === 'highest_score') {
+            return data.sort((a, b) => b.data.vote_average - a.data.vote_average);
+        }
+        if (config.filtering === 'lowest_score') {
+            return data.sort((a, b) => a.data.vote_average - b.data.vote_average);
+        }
+        if (config.filtering === 'last_added') {
+            return data.sort((a, b) => b.timestamp - a.timestamp);
+        }
+        if (config.filtering === 'first_added') {
+            return data.sort((a, b) => a.timestamp - b.timestamp);
+        }
+    };
+
 };
 
 export default Helper;
