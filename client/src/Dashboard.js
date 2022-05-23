a6d45cc2c2142ba180c12511e5da9b85d8733ab2
victordrumond <victordrumond@hotmail.com>
2022-05-23 01:20:38 -0300

    Add logo to dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import './Dashboard.css';
+import Requests from './Requests';
+import { useAuth0 } from '@auth0/auth0-react';
+import logo from './logo.png';
 import Search from './Search';
 import Main from './Main';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import { useAuth0 } from '@auth0/auth0-react';
-import Requests from './Requests';
 
 function Dashboard() {
 
@@ -53,7 +54,12 @@ function Dashboard() {
     return (
         <Container id="dashboard-container">
             <div id="header-container" className="d-flex justify-content-between">
-                <h2>React Movie App</h2>
+                <div id="name-and-logo" className="d-flex">
+                    <h2>React Movie App</h2>
+                    <div className="d-flex">
+                        <img src={logo} alt="app_logo" className="img-fluid" />
+                    </div>
+                </div>
                 <div id="logout-wrapper" className="d-flex flex-column">
                     <p>Welcome back, <b>{user.email}</b>!</p>
                     <Button id="logout" variant="primary" onClick={() => logout()}>
@@ -62,7 +68,7 @@ function Dashboard() {
                 </div>
             </div>
             <Search user={user} updateDashboard={updatedFromSearch} />
-            <Main user={user} userData={userData} updateDashboard={updatedFromMain}/>
+            <Main user={user} userData={userData} updateDashboard={updatedFromMain} />
             <div id="footer-container" className="d-flex align-items-center justify-content-center">
                 <p>&#169; 2022 React Movie App | A project by Victor</p>
             </div>
