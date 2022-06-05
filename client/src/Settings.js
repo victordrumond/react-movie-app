f437f4fcc7e96df4000067006f634642d19e4eac
victordrumond <victordrumond@hotmail.com>
2022-06-05 02:28:52 -0300

    Update sidebar & settings styling

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -3,6 +3,7 @@ import './Settings.css';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 import { CgProfile } from 'react-icons/cg';
 import { useAuth0 } from '@auth0/auth0-react';
 import Requests from './Requests';
@@ -12,20 +13,21 @@ function Settings() {
 
     const { user, getAccessTokenSilently } = useAuth0();
     const [userProfileUpdated, setUserProfileUpdated] = useState(false);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         if (userProfileUpdated) {
             getAccessTokenSilently({ ignoreCache: true });
             setUserProfileUpdated(false);
+            setLoading(false);
         }
     }, [userProfileUpdated, getAccessTokenSilently]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            if (loading) {
-                setLoading(false);
+            if (!loading) {
+                setLoading(null);
             };
         }, 5000)
         return () => clearTimeout(timer)
@@ -38,11 +40,15 @@ function Settings() {
             setError('Invalid username');
             return;
         }
+        if (!validateFormData(name, nickname, picture)) {
+            setError('Please edit at least one field');
+            return;
+        }
+        setError(null);
         setLoading(true);
         await getAccessTokenSilently().then(token => {
             Requests.editUserProfile(token, user.sub, name, nickname, picture)
                 .then(res => {
-                    setError(null);
                     setUserProfileUpdated(true);
                 })
                 .catch(error => {
@@ -52,11 +58,27 @@ function Settings() {
         })
     }
 
+    const validateFormData = (name, nickname, picture) => {
+        return (name !== user.name || nickname !== user.nickname || picture !== user.picture) ? true : false;
+    }
+
+    const getButtonVariant = () => {
+        if (loading === null) return 'primary';
+        if (loading) return 'primary';
+        if (!loading) return 'success'
+    }
+
+    const getButtonInnerHTML = () => {
+        if (loading === null) return 'Save';
+        if (loading) return '';
+        if (!loading) return 'Done!'
+    }
+
     return (
         <Container id="settings-container">
-            <div className="d-flex justify-content-start">
+            <div id="profile-title" className="d-flex justify-content-start">
                 <CgProfile className="settings-section-icon" />
-                <p>Profile</p>
+                <p>Profile Settings</p>
             </div>
             <Form id="user-settings-form" onSubmit={(e) => handleSubmit(e)}>
                 <Form.Group className="mb-2" controlId="formEmail">
@@ -77,8 +99,16 @@ function Settings() {
                     <Form.Control type="url" defaultValue={user.picture} />
                 </Form.Group>
                 <div className="pt-2 d-flex justify-content-start align-items-center">
-                    <Button id="update-profile-btn" variant={loading ? "success" : "primary"} type="submit" >
-                        {loading ? "Done!" : "Save"}
+                    <Button id="update-profile-btn" className="d-flex justify-content-center align-items-center" variant={getButtonVariant()} type="submit" >
+                        {getButtonInnerHTML()}
+                        {loading &&
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />}
                     </Button>
                     {error && <p id="error-msg">{error}</p>}
                 </div>
